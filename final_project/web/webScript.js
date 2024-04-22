ed2eb7ae7845ac77a8acc9252ae084a65129956c
derekchen <derekzongnanchen@gmail.com>
2024-04-21 17:35:06 -0700

    login page update

--- final_project/web/webScript.js
+++ final_project/web/webScript.js
@@ -1,5 +1,13 @@
-async function myButton() {
-    const response = await fetch("https://derekzongnan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/");
-    const movies = await response.json();
-    console.log(movies);
-  }
+// async function myButton() {
+//     const response = await fetch("https://derekzongnan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/");
+//     const movies = await response.json();
+//     console.log(movies);
+//   }
+
+async function login() {
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  url = "";
+  const response = await fetch(url+`/login?username={username}&password={passowrd}`);
+  document.getElementById("UsernameSpan").innerHTML = response.json();
+}
